cc1dbf9b5813e79eddb76af3a8126ee41ecdfca1
lorenzfischer <lorenz.fischer@gmail.com>
2011-10-01 14:22:56 +0200

    new layout

--- frontend/js/tag-cloud.js
+++ frontend/js/tag-cloud.js
@@ -1,3 +1,22 @@
+function addTagMap($isotope_container, tag_map) {
+    var min = 0,
+        max = 0,
+        diff = 0,
+        fct = 0;
+        
+    for (var tag in tag_map) { // find range of values
+        var value = tag_map[tag];
+        if (value < min) min = value;
+        if (value > max) max = value;
+    }
+    diff = max - min;
+    fct = 6 / diff; // we want values between 1 and 6
+    
+    for (var tag in tag_map) {
+        addTag($isotope_container, tag, tag_map[tag] * fct);
+    }
+}
+
 function addTag($isotope_container, tag, weight) {
       var $text_element = $('<h2 style="font-size: '+ weight +'em;">' + tag + '</h2>')
       //console.log($text_element.css('width'))
@@ -8,7 +27,7 @@ function addTag($isotope_container, tag, weight) {
       $isotope_container.isotope( 'insert', $newItem );
       $newItem.width($text_element.width() + 10);
       $newItem.height($text_element.height() + 10);
- }
+}
  
 function removeAllTags($isotope_container) {
     tagSet = $isotope_container.find('.element');
