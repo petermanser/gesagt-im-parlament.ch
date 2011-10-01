016d9db049d3311273dc526cb593a8aca4fa7cb1
lorenzfischer <lorenz.fischer@gmail.com>
2011-10-01 11:44:40 +0200

    moved js code into separate file

--- frontend/js/tag-cloud.js
+++ frontend/js/tag-cloud.js
@@ -1,11 +1,17 @@
 function addTag($isotope_container, tag, weight) {
-    var $text_element = $('<h2 style="font-size: '+ weight +'em;">' + tag + '</h2>')
-    //console.log($text_element.css('width'))
-            
-    var $newItem = $('<div class="element" ></div>');
-    $newItem.append($text_element);
+      var $text_element = $('<h2 style="font-size: '+ weight +'em;">' + tag + '</h2>')
+      //console.log($text_element.css('width'))
+
+      var $newItem = $('<div id="$'+ tag +'" class="element" ></div>');
+      $newItem.append($text_element);
+
+      $isotope_container.isotope( 'insert', $newItem );
+      $newItem.width($text_element.width() + 10);
+      $newItem.height($text_element.height() + 10);
+ }
+ 
+function removeAllTags($isotope_container) {
+    // tagSet = $($isotope_container ' .element');
+    //         console.log(tagSet)
     
-    $isotope_container.isotope( 'insert', $newItem );
-    $newItem.width($text_element.width() + 10);
-    $newItem.height($text_element.height() + 10);
 }
