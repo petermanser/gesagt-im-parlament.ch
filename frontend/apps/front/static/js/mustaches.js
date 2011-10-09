240ab75cd70e9056dd36af7da5f9e12b419b6c58
Danilo Bargen <gezuru@gmail.com>
2011-10-10 01:00:39 +0200

    Improved version of jQuery ctrl function

--- frontend/apps/front/static/js/mustaches.js
+++ frontend/apps/front/static/js/mustaches.js
@@ -1,17 +1,12 @@
 base_uri = 'http://gesagt-im-parlament.ep.io'
 
 $.ctrl = function(key, callback, args) {
-    var isCtrl = false;
     $(document).keydown(function(e) {
         if(!args) args=[]; // IE barks when args is null
-
-        if(e.ctrlKey) isCtrl = true;
-        if(e.keyCode == key.charCodeAt(0) && isCtrl) {
+        if(e.keyCode == key.charCodeAt(0) && e.ctrlKey) {
             callback.apply(this, args);
             return false;
         }
-    }).keyup(function(e) {
-        if(e.ctrlKey) isCtrl = false;
     });
 };
 
