d5880d42c746eec544a2a5e8c79f1b1c5e996349
Danilo Bargen <gezuru@gmail.com>
2011-10-10 00:59:15 +0200

    Add mustaches on ctrl+m

--- frontend/apps/front/static/js/mustaches.js
+++ frontend/apps/front/static/js/mustaches.js
@@ -1,7 +1,22 @@
-base_uri = 'http://dev.gesagt-im-parlament.ch'
+base_uri = 'http://gesagt-im-parlament.ep.io'
+
+$.ctrl = function(key, callback, args) {
+    var isCtrl = false;
+    $(document).keydown(function(e) {
+        if(!args) args=[]; // IE barks when args is null
+
+        if(e.ctrlKey) isCtrl = true;
+        if(e.keyCode == key.charCodeAt(0) && isCtrl) {
+            callback.apply(this, args);
+            return false;
+        }
+    }).keyup(function(e) {
+        if(e.ctrlKey) isCtrl = false;
+    });
+};
 
 $(document).ready(function() {
-    $('#V').click(function() {
+    $.ctrl('m', function() {
         $('.person img').each(function() {
             src = $(this).attr('src');
             $(this).attr('src', 'http://mustachify.me/?src=' + base_uri + src);
